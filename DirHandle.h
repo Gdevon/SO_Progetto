2a459a50f7e447ea1d460f41b8517ff52ed4b866
GDevoto <giacomodevoto9+github@gmail.com>
2025-08-02 16:49:35 +0200

    aggiunta struttura DirHandle

---
+++ DirHandle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+#include "FS_info.h"
+#include "FAT_info.h"
+#include "DIR_info.h"
+typedef struct {
+    Dir_Entry* entries;    
+    uint32_t num_entries;        
+    uint32_t position;     
+    uint16_t first_block;  
+    uint8_t open;          
+} DirectoryHandle;
